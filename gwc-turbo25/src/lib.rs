040e659b7e3212f3e154e50bdf84e168a59f6c36
Nathan <nobermiller16@gmail.com>
2025-05-17 21:21:35 -0700

    added the shrinking circle

--- gwc-turbo25/src/lib.rs
+++ gwc-turbo25/src/lib.rs
@@ -8,19 +8,30 @@ turbo::config!{
 turbo::init! {
     struct GameState {
         frame: u32,
+        time_elapsed: f64,
         started: bool,
         event_frequency: u32,
-        study_event_active: bool,
+
+        eat_event_active: bool,
+        eat_event_start_time: f64,
+        eat_event_timer: f64,
+        eat_event_time_left: f64,
     } = Self {
         frame: 0,
+        time_elapsed: 0.0,
         started: false,
         event_frequency: 50,
-        study_event_active: false,
+
+        eat_event_active: false,
+        eat_event_start_time:0.0,
+        eat_event_timer: 3.0,
+        eat_event_time_left: 0.0,
     }
 }
 
 turbo::go!{
     let mut state = GameState::load();
+    crate::println!("{:?} {}", {}, state.eat_event_time_left/state.eat_event_timer);
     if !state.started {
         
         let pointer = pointer();
@@ -48,25 +59,49 @@ turbo::go!{
         );
         let event_frequency = state.event_frequency;
         let spawn_event_val = rand() % event_frequency;
-        if spawn_event_val == 2 && state.study_event_active == false {
-            state.study_event_active = true;
+        if spawn_event_val == 2 && state.eat_event_active == false {
+            state.eat_event_active = true;
+            state.eat_event_start_time = state.time_elapsed;
         }
-        if state.study_event_active == true {
+        if state.eat_event_active == true {
+            state.eat_event_time_left = state.eat_event_timer + state.eat_event_start_time - state.time_elapsed;
+            //crate::println!("{:?} {}", {}, state.time_elapsed);
+            //if the timer is up, make you lose health and make the icon disappear
+            if state.eat_event_time_left <= 0.0 || state.eat_event_time_left > state.eat_event_start_time{
+                state.eat_event_active = false;
+                //TAKE DAMAGE
+            }
             //When the event is active, make it show the icon
+            circ!(
+                d = 28.0 + (state.eat_event_time_left/state.eat_event_timer)*40.0,
+                x = 25.0 + (state.eat_event_timer - state.eat_event_time_left) * 7.0,
+                y = -6.0 + (state.eat_event_timer - state.eat_event_time_left) * 7.0,
+                
+                color = 0xadd8e6,
+            );
             sprite!("button_eat_hotdog",
                 x = 40,
                 y = 10,
                 scale = 0.3,
             );
+            text!("EAT",
+                x = 52,
+                y = 3,
+            );
+            text!("W",
+                x = 56,
+                y = 45,
+                font = "large",
+            );
         }
-        if state.study_event_active == true && gamepad(0).up.pressed() {
-            state.study_event_active = false;
-            
+        if state.eat_event_active == true && gamepad(0).up.pressed() {
+            state.eat_event_active = false;
         }
 
 
     }
     state.frame += 1;
+    state.time_elapsed = state.frame as f64 / 60.0;
     state.save();
 }
 
